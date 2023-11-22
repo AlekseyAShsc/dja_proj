f0d0467f849acceaa8f0e685e2c75651b2147afa
Aleksey Shcherbina <aleksey@ok-autoparts.ru>
2023-11-22 16:31:44 +0500

    Урок 31

--- products/models.py
+++ products/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
-    catogory = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Продукт: {self.name} | Категория: {self.catogory.name}'
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
